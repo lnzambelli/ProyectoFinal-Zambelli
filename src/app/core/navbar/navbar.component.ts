7dcbe0908149c53510e7abd4d1eba57d2b45a748
Luis Zambelli <lnzambelli@gmail.com>
2022-06-21 23:15:00 -0300

    se agrego ruta activa en el navbar

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -9,11 +10,18 @@ import { Router } from '@angular/router';
 export class NavbarComponent implements OnInit {
 
   nombreUsuario: string =""
+  nombreUrl: any
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,  private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.nombreUsuario = sessionStorage.getItem('usuario') || "";
+    this.router.events.pipe(
+      filter((event: any) => event instanceof NavigationEnd)
+  )
+      .subscribe(event => {
+          this.nombreUrl = event.url;
+      });
   }
 
 }
